f9ef996aeb320659383c230a2a5a0cba66ebf946
Vitória <gitgitt0@gmail.com>
2022-02-15 18:39:23 -0300

    Initial commit

---
+++ Desafio aula 1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Desafio_aula_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.lblNumero1 = new System.Windows.Forms.Label();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.txtSoma = new System.Windows.Forms.TextBox();
+            this.lblSoma = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(121, 200);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 0;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(262, 200);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(202, 75);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 23);
+            this.txtNumero1.TabIndex = 2;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(202, 117);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 23);
+            this.txtNumero2.TabIndex = 3;
+            // 
+            // lblNumero1
+            // 
+            this.lblNumero1.AutoSize = true;
+            this.lblNumero1.Location = new System.Drawing.Point(136, 78);
+            this.lblNumero1.Name = "lblNumero1";
+            this.lblNumero1.Size = new System.Drawing.Size(60, 15);
+            this.lblNumero1.TabIndex = 4;
+            this.lblNumero1.Text = "Número 1";
+            this.lblNumero1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Location = new System.Drawing.Point(136, 120);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(60, 15);
+            this.lblNumero2.TabIndex = 5;
+            this.lblNumero2.Text = "Número 2";
+            // 
+            // txtSoma
+            // 
+            this.txtSoma.Location = new System.Drawing.Point(202, 158);
+            this.txtSoma.Name = "txtSoma";
+            this.txtSoma.Size = new System.Drawing.Size(100, 23);
+            this.txtSoma.TabIndex = 6;
+            // 
+            // lblSoma
+            // 
+            this.lblSoma.AutoSize = true;
+            this.lblSoma.Location = new System.Drawing.Point(149, 166);
+            this.lblSoma.Name = "lblSoma";
+            this.lblSoma.Size = new System.Drawing.Size(37, 15);
+            this.lblSoma.TabIndex = 7;
+            this.lblSoma.Text = "Soma";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSoma);
+            this.Controls.Add(this.txtSoma);
+            this.Controls.Add(this.lblNumero2);
+            this.Controls.Add(this.lblNumero1);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnCancelar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Label lblNumero1;
+        private System.Windows.Forms.Label lblNumero2;
+        private System.Windows.Forms.TextBox txtSoma;
+        private System.Windows.Forms.Label lblSoma;
+    }
+}
+
---
+++ Desafio aula 1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio_aula_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double n1, n2, soma;
+            if (txtNumero1.Text == string.Empty){
+                MessageBox.Show("Texto em branco na 1 caixa");
+            }
+            else
+            {
+                if (txtNumero2.Text == string.Empty)
+                {
+                    MessageBox.Show("Texto em branco na 2 caixa");
+                }
+            }
+            n1 = Convert.ToDouble(txtNumero1.Text);
+            n2 = Convert.ToDouble(txtNumero2.Text);
+            soma = n1 + n2;
+            txtSoma.Text = Convert.ToString(soma);
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtNumero1.Text = string.Empty;
+            txtNumero2.Text = string.Empty;
+            txtSoma.Text = string.Empty;
+        }
+    }
+}
